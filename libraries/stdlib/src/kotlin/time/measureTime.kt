cbbe22f85f0a82d6d3c08dd4ff2bbfa28f6f7b40
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2022-01-25 00:47:21 +0300

    Make TimeMark returned by TimeSource.Monotonic a value class
    This value class wraps Long on JVM and Native thus reducing allocations
    in time measurement scenarios when the default monotonic time source is
    statically known.
    
    KT-46132

--- libraries/stdlib/src/kotlin/time/measureTime.kt
+++ libraries/stdlib/src/kotlin/time/measureTime.kt
@@ -39,6 +39,23 @@ public inline fun TimeSource.measureTime(block: () -> Unit): Duration {
     return mark.elapsedNow()
 }
 
+/**
+ * Executes the given function [block] and returns the duration of elapsed time interval.
+ *
+ * The elapsed time is measured with the specified `this` [TimeSource.Monotonic] instance.
+ */
+@SinceKotlin("1.7")
+@ExperimentalTime
+public inline fun TimeSource.Monotonic.measureTime(block: () -> Unit): Duration {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    val mark = markNow()
+    block()
+    return mark.elapsedNow()
+}
+
 
 /**
  * Data class representing a result of executing an action, along with the duration of elapsed time interval.
@@ -75,6 +92,24 @@ public inline fun <T> measureTimedValue(block: () -> T): TimedValue<T> {
 @SinceKotlin("1.3")
 @ExperimentalTime
 public inline fun <T> TimeSource.measureTimedValue(block: () -> T): TimedValue<T> {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    val mark = markNow()
+    val result = block()
+    return TimedValue(result, mark.elapsedNow())
+}
+
+/**
+ * Executes the given [block] and returns an instance of [TimedValue] class, containing both
+ * the result of function execution and the duration of elapsed time interval.
+ *
+ * The elapsed time is measured with the specified `this` [TimeSource.Monotonic] instance.
+ */
+@SinceKotlin("1.7")
+@ExperimentalTime
+public inline fun <T> TimeSource.Monotonic.measureTimedValue(block: () -> T): TimedValue<T> {
     contract {
         callsInPlace(block, InvocationKind.EXACTLY_ONCE)
     }
