c0e6b9959c235815056529915f5558c239ea45fb
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-04-09 17:35:53 +0200

    [WASM] Minor refactorings

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/utils/Utils.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/utils/Utils.kt
@@ -6,11 +6,7 @@
 package org.jetbrains.kotlin.backend.wasm.utils
 
 import org.jetbrains.kotlin.ir.IrBuiltIns
-import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.expressions.IrTry
-import org.jetbrains.kotlin.ir.types.classOrNull
-import org.jetbrains.kotlin.ir.types.classifierOrFail
-import org.jetbrains.kotlin.ir.util.isInterface
 
 // Backed codegen can only handle try/catch in canonical form.
 // Canonical form for the try/catch:
@@ -23,19 +19,3 @@ import org.jetbrains.kotlin.ir.util.isInterface
 internal fun IrTry.isCanonical(builtIns: IrBuiltIns) =
     catches.singleOrNull()?.catchParameter?.symbol?.owner?.type == builtIns.throwableType &&
     finallyExpression == null
-
-internal fun IrClass.hasInterfaceForClass(): Boolean =
-    firstInterfaceForClass() != null
-
-internal fun IrClass.firstInterfaceForClass(): IrClass? {
-    var superClass: IrClass? = null
-    for (superType in superTypes) {
-        val typeAsClass = superType.classifierOrFail.owner as IrClass
-        if (typeAsClass.isInterface) {
-            return typeAsClass
-        } else {
-            superClass = typeAsClass
-        }
-    }
-    return superClass?.firstInterfaceForClass()
-}
